f9407acf41ebd6e9c7e4c20d604f512bcb1b6bff
sbondada <sbondada@buffalo.edu>
2014-03-06 00:34:38 -0500

    added a simple and a trditinal code to off  the right most set bit

--- bits/TurnOffRightMostSetBIt.java
+++ bits/TurnOffRightMostSetBIt.java
@@ -2,9 +2,27 @@ package bits;
 
 public class TurnOffRightMostSetBIt 
 {
-
+	public int turnoffRightmostsetbit(int num)
+	{
+		int shifter=1;
+		while((num&shifter)==0)
+		{
+			shifter=shifter<<1;
+		}
+		return (num^shifter);
+	}
+	public int turnoffrightmostsetbit(int num)
+	{
+		return (num&(num-1));
+	}
 	public static void main(String args[])
 	{
-		
+		TurnOffRightMostSetBIt trmsb = new TurnOffRightMostSetBIt();
+		int num=0b1100011011000;
+		int offnum=trmsb.turnoffRightmostsetbit(num);
+		int roffnum=trmsb.turnoffrightmostsetbit(num);
+		System.out.println("orginal num "+Integer.toBinaryString(num));
+		System.out.println("R off num "+Integer.toBinaryString(offnum));
+		System.out.println("r off num "+Integer.toBinaryString(roffnum));
 	}
 }
